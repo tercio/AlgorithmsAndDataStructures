5520933b293b96f269b0299c5f83dc4d44e26c82
Tercio Esperandio <tercio@mpc.com.br>
2021-09-28 20:40:33 -0300

    fix

--- is_rotation.py
+++ is_rotation.py
@@ -7,8 +7,8 @@ def isRotation (s1,s2):
 
     if len(s1) == len(s2) and len(s1) > 0:
 
-        s1s2 = s1 + s2
-        return s2 in s1s2
+        s1s1 = s1 + s1
+        return s2 in s1s1
 
     return False
 
